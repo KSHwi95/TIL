243ebbfda4d873914e3dfd14aaeb2d5decf53052
KSHwi95 <bassstg@naver.com>
2023-04-16 19:40:50 +0900

    0416

---
+++ 코드연습장/Human.py
@@ -0,0 +1,9 @@
+#모듈 생성 %%writefile ____.py
+class CreateHuman: 
+    __eye__='눈은 두개'
+    __ear__='귀도 두개'
+    nation = 'South Korea'
+    def __init__(self,name,age):
+        self.name=name
+        self.age=age
+ 
